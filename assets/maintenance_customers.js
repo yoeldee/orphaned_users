33bb18461cec7352733093a44cbf3cc0ce3f76ad
Yoel <yoeld@gmx.net>
2023-09-04 23:09:44 +0300

    remove console bloat

--- assets/maintenance_customers.js
+++ assets/maintenance_customers.js
@@ -25,7 +25,6 @@ function addRowToTable(index,entity_id,entity_name,entity_type,entity_managing,e
 	var table = document.getElementById(tableType);
 	var row = table.insertRow(-1);
     var row_checkbox = row.insertCell(0);
-	//var row_index = row.insertCell(1);
 	var row_entity_id = row.insertCell(1);
 	var row_entity_name = row.insertCell(2);
     var row_entity_type = row.insertCell(3);
@@ -33,7 +32,6 @@ function addRowToTable(index,entity_id,entity_name,entity_type,entity_managing,e
 	var row_entity_created_at = row.insertCell(5);
 	var row_entity_updated_at = row.insertCell(6);
     row.id = "row_"+entity_id;
-	//  row_index.innerHTML = "";
 	row_entity_id.innerHTML = entity_id;
 	row_entity_name.innerHTML = entity_name;
     row_entity_name.id = 'name_'+entity_id;
@@ -48,12 +46,8 @@ async function getOrphanedUsers() {
     var usersCount = 1 ;
     const systemURL = await getSystemURL();
     for (page=1; page<=usersCount; page++){
-        console.log('page :', page);
-
         var x = await client.request('/api/v2/users?page='+page);
         usersCount = Math.ceil(x.count/100)
-        console.log('usersCount :', usersCount);
-
         for (let i = 0; i < x.users.length; i++) {
             if (x.users[i].organization_id == null){
                 addRowToTable(i+1,x.users[i]['id'],"<a href=\""+systemURL+"/users/"+x.users[i]['id']+"\" target =\"_blank\">"+x.users[i]['name']+"</a>"+"  -  "+x.users[i]['email'],'','',
